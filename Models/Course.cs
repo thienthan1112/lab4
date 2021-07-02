60a6e355fdf9df5b681d5bd54685d9a2c6c81d44
thienthan1112 <trungbutnguyen1811@gmail.com>
2021-07-02 19:30:56 +0700

    Initial commit

---
+++ Models/Course.cs
@@ -0,0 +1,31 @@
+namespace lab4.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string LecturerId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Name;
+
+        public virtual Category Category { get; set; }
+        public List<Category> ListCategory = new List<Category>();
+    }
+}
